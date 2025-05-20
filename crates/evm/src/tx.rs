1c4f35ca45a4d32ec6929be0943ff59618fe8088
stevencartavia <112043913+stevencartavia@users.noreply.github.com>
2025-05-20 14:14:53 -0600

    feat: Implement from_recovered_tx for txDeposit nativel (#96)
    * feat: Implement from_recovered_tx for txDeposit nativel
    
    * update TxDeposit

--- crates/evm/src/tx.rs
+++ crates/evm/src/tx.rs
@@ -363,27 +363,31 @@ mod op {
                 OpTxEnvelope::Eip1559(tx) => Self::from_recovered_tx(tx.tx(), caller),
                 OpTxEnvelope::Eip2930(tx) => Self::from_recovered_tx(tx.tx(), caller),
                 OpTxEnvelope::Eip7702(tx) => Self::from_recovered_tx(tx.tx(), caller),
-                OpTxEnvelope::Deposit(tx) => {
-                    let TxDeposit {
-                        to,
-                        value,
-                        gas_limit,
-                        input,
-                        source_hash: _,
-                        from: _,
-                        mint: _,
-                        is_system_transaction: _,
-                    } = tx.inner();
-                    Self {
-                        tx_type: tx.ty(),
-                        caller,
-                        gas_limit: *gas_limit,
-                        kind: *to,
-                        value: *value,
-                        data: input.clone(),
-                        ..Default::default()
-                    }
-                }
+                OpTxEnvelope::Deposit(tx) => Self::from_recovered_tx(tx.inner(), caller),
+            }
+        }
+    }
+
+    impl FromRecoveredTx<TxDeposit> for TxEnv {
+        fn from_recovered_tx(tx: &TxDeposit, caller: Address) -> Self {
+            let TxDeposit {
+                to,
+                value,
+                gas_limit,
+                input,
+                source_hash: _,
+                from: _,
+                mint: _,
+                is_system_transaction: _,
+            } = tx;
+            Self {
+                tx_type: tx.ty(),
+                caller,
+                gas_limit: *gas_limit,
+                kind: *to,
+                value: *value,
+                data: input.clone(),
+                ..Default::default()
             }
         }
     }
