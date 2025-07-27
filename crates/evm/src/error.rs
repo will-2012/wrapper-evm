50733d7135a8cd463ca134c679a45a89702346ae
stevencartavia <112043913+stevencartavia@users.noreply.github.com>
2025-07-27 04:28:55 -0600

    feat: add as_invalid_tx_err in InvalidTxError (#143)
    * feat: add as_invalid_tx_err in InvalidTxError
    
    * chore: touchups
    
    ---------
    
    Co-authored-by: Matthias Seitz <matthias.seitz@outlook.de>

--- crates/evm/src/error.rs
+++ crates/evm/src/error.rs
@@ -7,11 +7,20 @@ use revm::context_interface::result::{EVMError, InvalidTransaction};
 pub trait InvalidTxError: Error + Send + Sync + Any + 'static {
     /// Returns whether the error cause by transaction having a nonce lower than expected.
     fn is_nonce_too_low(&self) -> bool;
+
+    /// Returns the underlying [`InvalidTransaction`] if any.
+    ///
+    /// This is primarily used for error conversions, e.g. for rpc responses.
+    fn as_invalid_tx_err(&self) -> Option<&InvalidTransaction>;
 }
 
 impl InvalidTxError for InvalidTransaction {
     fn is_nonce_too_low(&self) -> bool {
         matches!(self, Self::NonceTooLow { .. })
+    }
+
+    fn as_invalid_tx_err(&self) -> Option<&InvalidTransaction> {
+        Some(self)
     }
 }
 
@@ -66,5 +75,12 @@ where
 impl InvalidTxError for op_revm::OpTransactionError {
     fn is_nonce_too_low(&self) -> bool {
         matches!(self, Self::Base(tx) if tx.is_nonce_too_low())
+    }
+
+    fn as_invalid_tx_err(&self) -> Option<&InvalidTransaction> {
+        match self {
+            Self::Base(tx) => Some(tx),
+            _ => None,
+        }
     }
 }
